525623f8a53f7063ae79fea8c01be6c75a279475
zenghou <zenghoulim@outlook.com>
2019-04-01 23:33:31 +0800

    Listen to event emitter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'todoList';
+  menuItems: string[];
+  currSelected: string;
+
+  ngOnInit() {
+    this.menuItems = ['Incomplete', 'Complete'];
+    this.currSelected = this.menuItems[0];
+  }
+
+  ngAfterViewInit() {
+  }
+
+  onSelected(selectedItem) {
+    this.currSelected = selectedItem;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import * as $ from 'jquery';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SlidingUnderlineMenuComponent } from './sliding-underline-menu/sliding-underline-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SlidingUnderlineMenuComponent
   ],
   imports: [
     BrowserModule,
